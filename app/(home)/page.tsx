90e3d59031663f1c26034f080b48b4e7e6cf4ca1
Sakshi <sakshisingh9906@gmail.com>
2025-04-04 20:23:27 +0530

    10-create-browsing-history

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,3 +1,4 @@
+import BrowsingHistoryList from '@/components/shared/browsing-history-list'
 import { HomeCard } from '@/components/shared/home/home-card'
 import { HomeCarousel } from '@/components/shared/home/home-carousel'
 import ProductSlider from '@/components/shared/product/product-slider'
@@ -71,23 +72,27 @@ export default async function Page() {
       <HomeCarousel items={data.carousels} />
       <div className='md:p-4 md:space-y-4 bg-border'>
         <HomeCard cards={cards} />
+
+        <Card className='w-full rounded-none'>
+          <CardContent className='p-4 items-center gap-3'>
+            <ProductSlider title={"Today's Deals"} products={todaysDeals} />
+          </CardContent>
+        </Card>
+
+        <Card className='w-full rounded-none'>
+          <CardContent className='p-4 items-center gap-3'>
+            <ProductSlider
+              title='Best Selling Products'
+              products={bestSellingProducts}
+              hideDetails
+            />
+          </CardContent>
+        </Card>
       </div>
-
-      <Card className='w-full rounded-none'>
-        <CardContent className='p-4 items-center gap-3'>
-          <ProductSlider title={"Today's Deals"} products={todaysDeals} />
-        </CardContent>
-      </Card>
-
-      <Card className='w-full rounded-none'>
-        <CardContent className='p-4 items-center gap-3'>
-          <ProductSlider
-            title='Best Selling Products'
-            products={bestSellingProducts}
-            hideDetails
-          />
-        </CardContent>
-      </Card>
+      
+      <div className='p-4 bg-background'>
+        <BrowsingHistoryList />
+      </div>
     </>
   )
 }
